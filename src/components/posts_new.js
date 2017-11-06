640ad2a6c0434c686050fd45a5535f13bc877397
Ji Kim <jkim55@gmail.com>
2017-11-05 17:54:58 -0700

    add new posts page component and route; using Switch component and organized routes components accordingly

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        New Posts Stubbed
+      </div>
+    )
+  }
+}
+
+export default PostsNew
