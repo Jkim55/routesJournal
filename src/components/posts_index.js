872a37423910b13c02ea39a8cf3873104a8e0d1f
Ji Kim <jkim55@gmail.com>
2017-11-09 22:04:46 -0700

    testing out performance stuff

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { fetchPosts } from "../actions/fetch_posts";
 import _ from 'lodash'
+
 class PostsIndex extends Component {
   componentDidMount() {
     this.props.fetchPosts()
@@ -10,12 +12,22 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new" onClick={() => this.create_mark("startCrossPage")} >
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className='list-group'>
           {this.renderPosts()}
         </ul>
       </div>
     )
+  }
+
+  create_mark(name) {
+    console.log(`CREATING MARK FOR: ${name}`)
+    performance.mark !== undefined ? performance.mark(name): console.log("performance.mark Not supported")
   }
 
   renderPosts(){
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,10 +1,78 @@
 import React, { Component } from 'react'
 
 class PostsNew extends Component {
+  componentDidMount() {
+    this.create_mark("endCrossPage");
+    this.initTask("SinglePage")
+
+    this.crossPageMark("startCrossPage")
+    this.showMarks()
+    this.showMeasures()
+    this.clearAllPerfMetrics()
+  }
+
+  initTask(taskName) {
+    performance.mark(`start${taskName}`);
+    performance.mark(`end${taskName}`);
+    this.measurePerf(taskName, `start${taskName}`, `end${taskName}`)
+  }
+
+  crossPageMark(startMark){
+    const marks = performance.getEntriesByType("mark")
+    let matched = marks.filter(mark => mark.name === startMark)
+    matched.length > 0 ? this.singlePerfMeasure("CrossPage", startMark, "endCrossPage") : console.log("NO MARKS FOR ", startMark)
+  }
+
+  create_mark(name) {
+    console.log(`CREATING MARK FOR: ${name}`)
+    performance.mark !== undefined ? performance.mark(name): console.log("performance.mark Not supported")
+  }
+
+  measurePerf(taskName, startMark, endMark) {
+    performance.measure(taskName, startMark, endMark)
+    const measured = performance.getEntriesByName(taskName)[0];
+    console.log(`NAME: ${measured.name}\n TYPE: ${measured.entryType}\n START TIME: ${measured.startTime}\n DURATION: ${measured.duration*0.001}`);
+  }
+
+  singlePerfMeasure(){
+      const marks = performance.getEntriesByType("mark");
+      marks.map(mark => {
+        console.log(`SINGLE PERF FOR MARKS\n NAME: ${mark.name}\n TYPE: ${mark.entryType}\n STARTTIME: ${mark.startTime}\n DURATION: ${mark.duration*0.001}\n`)
+      })
+  }
+
+  showMarks(){
+    const marks = performance.getEntriesByType("mark");
+    if (marks.length === 0){
+      console.log("NO MARKS")
+    }
+    marks.map(mark => {
+      console.log(`MARKS LEFTOVER\n NAME: ${mark.name}`)
+    })
+  }
+
+  showMeasures(){
+    const measures=performance.getEntriesByType("measure");
+    if (measures.length === 0){
+      console.log("NO MEASURES")
+    }
+    measures.map(measure => {
+      console.log(`MEASURES LEFTOVER\n NAME: ${measure.name}`)
+    })
+  }
+
+  clearAllPerfMetrics(){
+    performance.clearMarks();
+    performance.clearMeasures();
+    console.log("CLEARING!!!**************")
+    this.showMarks()
+    this.showMeasures()
+  }
+
   render() {
     return (
       <div>
-        New Posts Stubbed
+        Psssst.. take a look in the console!
       </div>
     )
   }
